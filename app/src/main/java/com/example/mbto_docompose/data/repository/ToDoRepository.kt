600e6f0801dd9b54878b842c6a9523797e9403cd
MehdiBahrami <mbahrami900913@gmail.com>
2024-11-18 11:32:09 +0330

    Create SharedViewModel

--- app/src/main/java/com/example/mbto_docompose/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/mbto_docompose/data/repository/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.example.mbto_docompose.data.repository
 
 import com.example.mbto_docompose.data.ToDoDao
 import com.example.mbto_docompose.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
